3335604736b3ae44b0c3ba70cb748b11bf1da367
droll-accelerator-1483 <srsani@gmail.com>
2019-06-21 08:03:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(obj, key, value){
-  
-  obj[key] = value
-  return obj
+  var newObj = Object.assign({}, obj)
+  newObj[key] = value
+  return newObj
 }
