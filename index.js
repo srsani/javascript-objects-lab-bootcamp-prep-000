fd9bebb17559e92a26d6a4040800a6b3ffb4658c
droll-accelerator-1483 <srsani@gmail.com>
2019-06-21 08:03:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ function updateObjectWithKeyAndValue(obj, key, value){
   return newObj
 }
 
-function updateObjectWithKeyAndValue(obj, key, value){
-  var newObj = Object.assign({}, obj)
-  newObj[key] = value
-  return newObj
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
+  obj[key] = value
+  return obj
 }
